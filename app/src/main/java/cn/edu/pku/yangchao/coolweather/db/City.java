34f00a6094d07713b18d02bb7bf5dcb4f112e5a6
tom_glb <good@qq.com>
2018-11-01 19:29:49 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/cn/edu/pku/yangchao/coolweather/db/City.java
@@ -0,0 +1,46 @@
+package cn.edu.pku.yangchao.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by YangChao on 2018/11/1.
+ */
+public class City extends DataSupport {
+
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
